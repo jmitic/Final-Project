e21e8e9d7b0e39aefaaf75ef4b5e7866829ab22a
Peter Brown <peter.brown@converse.edu>
2022-11-10 17:26:29 -0500

    Added dropdown menu filled from SQL

--- webapp/routes/index.js
+++ webapp/routes/index.js
@@ -3,17 +3,63 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let query = "SELECT * from Institution;"
-  req.app.locals.db.all(query, [], (err, rows) => {
+  let dropdown_query = 'SELECT id, schoolName from Institution;';
+  req.app.locals.db.all(dropdown_query, [], (err, schools) => {
     if (err) {
       throw err;
     }
+    //console.log('Schools: ' + schools);
+    let query = "SELECT * from Institution;"
+    req.app.locals.db.all(query, [], (err, rows) => {
+      if (err) {
+        throw err;
+      }
 
-    //console.log(rows);
-    res.render('index', { title: 'crsequiv database app', query: query, rows: rows });
+      //console.log(rows);
+      res.render('index', { title: 'crsequiv database app', query: query, 
+                  rows: rows, fillins: schools });
+    });
   });
 });
 
-//router.post('/', )
+router.post('/', function(req, res, next) {
+  let dropdown_query = 'SELECT id, schoolName from Institution;';
+  req.app.locals.db.all(dropdown_query, [], (err, schools) => {
+    if (err) {
+      throw err;
+    }
+    //console.log('Schools: ' + schools);
+    let query = '';
+    if (req.body.formname && (req.body.formname === 'SQL')) {
+      // Note this is HIGHLY insecure!  Input should be
+      // sanitized before being used as code!!
+      query = `SELECT ${req.body.columns} FROM `
+              + `${req.body.tablespec} WHERE `
+              + `${req.body.where_clause};`;
+    }
+    else if (req.body.formname && (req.body.formname == 'dropdown')) {
+      query = `SELECT ${req.body.columns} FROM Course WHERE `
+              + `instID = ${req.body.instID};`;
+    }
+    
+    if (query !== '') {
+      console.log(query);
+      req.app.locals.db.all(query, [], (err, rows) => {
+        if (err) {
+          throw err;
+        }
+
+        //console.log(rows);
+        res.render('index', { title: 'crsequiv database app', 
+            query: query, rows: rows, fillins: schools });
+
+      });
+    }
+    else { // No form data
+      res.render('index', { title: 'crsequiv database app', 
+                  query: '', rows: [], fillins: schools });
+    }
+  });
+});
 
 module.exports = router;
